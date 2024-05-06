3a3d040fb98658ab13bbef71038749250e8e3e7b
Honeyxslayer <163923981+Honeyxslayer@users.noreply.github.com>
2024-05-06 18:12:13 +0530

    HONEY

--- Honey/config.py
+++ Honey/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "OWNER_ID"
+    OWNER_ID = "5934011554"
     sudo_users = "7142432455", "5934011554"
     GROUP_ID = -1002090005496
-    TOKEN = "BOT_TOKEN"
-    mongo_url = "MONGO_DB_URL"
+    TOKEN = "6936772405:AAGLpttd9DAYPr75yCFphf1AbmiStbNub-8"
+    mongo_url = "mongodb+srv://honey:honey@honey.mf1aofb.mongodb.net/?retryWrites=true&w=majority&appName=honey"
     PHOTO_URL = ["https://graph.org/file/d5a2374e6027b72e92ae1.jpg", "https://graph.org/file/42cc1a7769dab55cbfa49.jpg"]
-    SUPPORT_CHAT = "SUPPORT_CHAT"
+    SUPPORT_CHAT = "honey_suppoort"
     UPDATE_CHAT = "Honey_networks"
-    BOT_USERNAME = "BOT_USERNAME"
+    BOT_USERNAME = "DbhsgGbot"
     CHARA_CHANNEL_ID = "-1002136697049"
     api_id = 29044160
-    api_hash = "API_HASH"
+    api_hash = "b93797389eab3cec8c697ae4f2418466"
 
     
 class Production(Config):
